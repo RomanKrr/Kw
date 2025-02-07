213fc1fdb23a03558fd1735dbec58bc2d7f92c7a
RomanKrr <158074611+RomanKrr@users.noreply.github.com>
2025-02-07 12:26:19 +0200

    d

--- backend/src/routes/products.route.js
+++ backend/src/routes/products.route.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { getProduct, getProductsByCategory } from "../controllers/product.controller";
+import { getProduct, getProductsByCategory } from "../controllers/product.controller.js";
 
 const router = express.Router()
 
